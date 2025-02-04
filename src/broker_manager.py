cb672052d2995d7dc7cda71f189fb77a783e981b
bathtimefish <muraoka@bathtimefish.com>
2025-02-04 16:21:58 +0900

    updated broker manager

--- src/broker_manager.py
+++ src/broker_manager.py
@@ -1,16 +1,15 @@
 from lib.broker import Broker
 
-#from brokers.simple_broker import SimpleBroker 
+from brokers.simple_broker import SimpleBroker 
 #from brokers.heartbeat_broker import HeartbeatBroker
-# test for brave jig helper
-from brokers.brave_jig_broker import BraveJigBroker
+#from brokers.brave_jig_broker import BraveJigBroker
 
 class BrokerManager:
 
     def __init__(self)->list[Broker]:
         # Add your brokers here
         self.brokers = [
-            #SimpleBroker(),
+            SimpleBroker(),
             #HeartbeatBroker(),
-            BraveJigBroker()
+            #BraveJigBroker()
         ]
