947d706ffcfa7a75c96f6fe809ff2bf6b3997ead
bathtimefish <muraoka@bathtimefish.com>
2025-02-09 17:02:50 +0900

    impl brave_jig brocker

---
+++ env.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PYTHONDONTWRITEBYTECODE=1
+export KRAKENB_DEBUG=1
+export KRAKENB_GRPC_HOST=[::]:50051
--- src/broker_manager.py
+++ src/broker_manager.py
@@ -1,15 +1,17 @@
 from lib.broker import Broker
 
-from brokers.simple_broker import SimpleBroker 
+#from brokers.simple_broker import SimpleBroker 
 #from brokers.heartbeat_broker import HeartbeatBroker
 #from brokers.brave_jig_broker import BraveJigBroker
+from brokers.test_jig_broker import TestJigBroker
 
 class BrokerManager:
 
     def __init__(self)->list[Broker]:
         # Add your brokers here
         self.brokers = [
-            SimpleBroker(),
+            #SimpleBroker(),
             #HeartbeatBroker(),
             #BraveJigBroker(),
+            TestJigBroker(),
         ]
